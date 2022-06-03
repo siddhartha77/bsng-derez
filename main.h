897ae8b89416c8e6800e8219bd47d714313362d2
siddhartha77 <77siddhartha@gmail.com>
2022-06-02 21:18:44 -0700

    Delete main.h

--- main.h
+++
@@ -1,1 +0,0 @@
-#ifndef	__DROPSHELL_H__#define	__DROPSHELL_H__void			Panic(void);void			InitToolbox(void);Boolean			InitGlobals(void);void			SetUpMenus(void);void			ShowAbout(void);void			DoMenu(long retVal);void			DoMouseDown(EventRecord *curEvent);void			DoKeyDown(EventRecord *curEvent);#endif
